469342187015a0fcb5df61f7a59b11c43821d84c
Bystrykha <tri_de@inbox.ru>
2021-03-19 21:10:48 +0700

    final vers

--- main.cpp
+++ main.cpp
@@ -2,81 +2,72 @@
 #include <fstream>
 #include <string>
 #include <queue>
+#include <regex>
+
 
 int numbers[3];
+std::string one_str_from_file;
+std::regex pattern_str("[0-9]+ [0-9]+ [1-9][0-9]+");
+std::regex pattern_word("[0-9]+");
+unsigned long long q, z;
+int n = 0, m;
 
-int numb_define(const std::string& dir){
-    std::ifstream file(dir);
-    std::string numb1, numb2, numb3;
-
-    if(file.is_open()){
-        file >> numb1 >> numb2 >> numb3;
-        numbers[0] = atoi(numb1.c_str());
-        numbers[1] = atoi(numb2.c_str());
-        numbers[2] = atoi(numb3.c_str());
-        return 1;
-    }
-
-    else{
-        std::cout << "The file is not open.";
-        return 0;
-    }
-}
-
-int from_right_to_left(){
-    int q = numbers[0], z, n = 0;
+void from_right_to_left(){
+    q = numbers[0];
+    m = numbers[2];
     std::queue <bool> bin_degree;
     for(int i = 0; numbers[1] != 0;){   //перевод в двоичную систему показателя степени
         i = numbers[1] % 2;
-        if(i == 0){
-            bin_degree.push(false);
-        }
-        else{
-            bin_degree.push(true);
-        }
+        if(i == 0) bin_degree.push(false);
+        else bin_degree.push(true);
         numbers[1] = numbers[1] / 2;
         n++;
     }
+    if(!bin_degree.front()) z = 1; // начальное значение z
+    else z = numbers[0];
 
-    if(!bin_degree.front()){
-        z = 1;
-    }
-    else {
-        z = numbers[0];
-    }
+    bin_degree.pop();   // убираем апервый элемент
 
-    bin_degree.pop();
-    for(int i = 1; i < n; i++){
-        q = (q * q) % numbers[2];
-        if(bin_degree.front()){
-            z = (z * q) % numbers[2];
-        }
+    for(int i = 1; i < n; i++){     // алгоритм справа на лево
+        q = (q * q) % m;
+        if(bin_degree.front()) z = (z * q) % m;
         bin_degree.pop();
     }
+    std::ofstream out;
+    out.open("output.txt", std::ios::app);
+    out << z << "\n";
+    z = 0;
+    q = 0;
+    m = 0;
+    n = 0;
+}
 
-    std::cout << z;
+int numb_define(const std::string& dir){
+    std::ifstream file;
+    file.open("input.txt");
+    if(file.is_open()){
+        while(getline(file, one_str_from_file)) {       //считывание из файла
+            if (std::regex_match(one_str_from_file, pattern_str)) {     // сравнение с регулярным выражением
+                for (int i = 0, j = 0; j < one_str_from_file.length(); j++) {     // разбиение валидной строки на числа
+                    if (one_str_from_file[j] >= '0' && one_str_from_file[j] <= '9') {
+                        numbers[i] = numbers[i] * 10 + (one_str_from_file[j] - 48);
+                    }
+                    else i++;
+                }
+                from_right_to_left();   // подсчет справа на лево
+            }
+            memset(numbers, 0, sizeof(int) * 3);    // обнуление массива
+        }
+    }
+    else{
+        std::cout << "pizdes" ;
+        return 0;
+    }
     return 1;
 }
 
-
 int main(){
-    std::string directory = "C:\\Users\\Asus\\Desktop\\aaa.txt";
-//    std::cin >> directory;
-    int f_res = numb_define(directory);
-
-    if(f_res == 1){
-//        std::cout << numbers[0] << numbers[1] << numbers[2];
-        for(int number : numbers) {
-            if (number < 0) {
-                std::cout << "One of numbers is negative.";
-                return 0;
-            }
-        }
-        int a = from_right_to_left();
-        return a;
-    }
-
-    else{
-        return 0;
-    }
+    std::string directory = "input.txt";
+    int res = numb_define(directory);
+    return res;
 }
